b530bf369bfc6e907ca3fc49cf2a14aa68b83078
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-06-12 11:34:25 +0300

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 use clap::{arg, command, ArgMatches};
 use std::{fs, io, process::exit};
 
+/// The default filename to use in case one isn't specified by the user
 const DEFAULT_FILENAME: &str = "main.bf";
 
+/// A recursive function that searches for loops within a BF file
 fn get_loop(code: &String, begin: usize, loops: &mut Vec<(usize, usize)>) -> usize {
+    // Begin reading the code from a parameter index
     let mut index = begin;
 
+    // Turn the code in a char Vec
     let chars = code.chars().collect::<Vec<char>>();
 
+    // Loop through each char in the Vec, beginning from the parameter index
     while index < chars.len() {
+        // Obtain the corresponding character
         let character = chars
             .get(index)
             .expect("Unexpected error while trying to index loops. Please report this error");
 
         match character {
+            // If it is the beginning of the loop, run the same function, BUT begin on a different index.
+            // Also, when the function is done executing, set the index to a later one to skip the already-processed loops
             '[' => index = get_loop(code, index + 1, loops),
+            // If it is the end of the loop, push a new loop tuple into the loops Vec and return with the current index
             ']' => {
                 loops.push((begin - 1, index));
                 return index;
@@ -22,12 +31,15 @@ fn get_loop(code: &String, begin: usize, loops: &mut Vec<(usize, usize)>) -> usi
             _ => (),
         };
 
+        // Increment index by one
         index += 1;
     }
 
+    // If no loop ending found, assume that the loop ending is at EOF
     chars.len()
 }
 
+/// Simulates a number overflow or underflow (used for the data pointer)
 fn wrapping_change(first: usize, add: bool, limit: usize) -> usize {
     if first == limit - 1 && add {
         0
@@ -42,6 +54,7 @@ fn wrapping_change(first: usize, add: bool, limit: usize) -> usize {
     }
 }
 
+/// Read a single [`char`] from the [`Stdin`](`io::Stdin`)
 fn read_char() -> char {
     let mut input = String::new();
 
@@ -57,20 +70,24 @@ fn read_char() -> char {
 }
 
 #[cfg(test)]
-mod tests {
+pub mod tests {
     use crate::get_loop;
 
+    /// A test function that ensures that the [`get_loop`] function works correctly
     #[test]
     fn test_loop_finder() {
+        // The cases against which to check
         const TEST_CASES: &[(&str, &[(usize, usize)])] = &[
             ("[]", &[(0, 1)]),
             (" []", &[(1, 2)]),
             ("[[] []]", &[(0, 6), (1, 2), (4, 5)]),
         ];
 
+        // Initialize some variables
         let mut loops: Vec<(usize, usize)> = Vec::new();
         let mut failed_cases: Vec<&str> = Vec::new();
 
+        // Run the tests. In case a test fails, DON'T PANIC, just push the failed case into the failed_cases Vec
         for (text, test_case) in TEST_CASES {
             loops.clear();
 
@@ -82,6 +99,7 @@ mod tests {
             }
         }
 
+        // If there is at least 1 fail, panic with a custom message
         if !failed_cases.is_empty() {
             panic!(
                 "A total of {} out of {} tests failed.\n\nThese are the cases that failed:\n{}",
@@ -97,6 +115,7 @@ mod tests {
 }
 
 fn main() {
+    // Obtain command line parameters
     let args: ArgMatches = command!()
         .about("A Brainf**k interpreter written in Rust with minimal dependencies")
         .arg(
@@ -106,8 +125,10 @@ fn main() {
         )
         .get_matches();
 
+    // Obtain the filename from them
     let filename = args.get_one::<String>("filename").expect("Error trying to obtain name of file to execute. This error shouldn't happen by default, since a default value is specified. Please report this error");
 
+    // Read file contents (or terminate if an error occurs while doing so)
     let code = fs::read_to_string(filename).unwrap_or_else(|error| {
         match error.kind() {
             io::ErrorKind::NotFound => eprintln!("File {} not found", filename),
@@ -122,8 +143,10 @@ fn main() {
 
     println!("Successfully opened file {}", filename);
 
+    // Initialize a Vec to store the loops' start and end
     let mut loops: Vec<(usize, usize)> = Vec::new();
 
+    // Obtain loops' data
     get_loop(&code, 0, &mut loops);
 
     // Allocate some memory for the data array, as well as the data pointer and the instruction pointer
@@ -132,8 +155,10 @@ fn main() {
 
     let mut data = [0 as u8; 30000];
 
+    // Turn the code in a char Vec
     let chars = code.chars().collect::<Vec<char>>();
 
+    // Loop through each character and process it accordingly
     while instruction_pointer < chars.len() {
         let character = chars
             .get(instruction_pointer)
